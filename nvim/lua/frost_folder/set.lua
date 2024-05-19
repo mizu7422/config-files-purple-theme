52ed1d85fe00668453367b5d9c05b1bfddea6650
mizu <ricardoreparaqui@gmail.com>
2024-05-19 16:52:42 +0100

    added nvim correctly

---
+++ nvim/lua/frost_folder/set.lua
@@ -0,0 +1,33 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.api.nvim_exec([[
+  autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 softtabstop=2
+  autocmd FileType typescript setlocal tabstop=2 shiftwidth=2 softtabstop=2
+]], false)
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "0"
+
+vim.g.mapleader = " "
